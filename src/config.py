4f9f09d2b22f3bea5aadc7b5b3e13ee69b2d9f74
keligou <94784230+keligou@users.noreply.github.com>
2021-11-21 15:26:20 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-content_folder = ['art', 'blog', 'journal']
+content_folder = ['art', 'blog', 'Journal']
 relative_build_url = "/"
 absolute_build_url = "https://keligou.github.io/Iglek/"
 build_folder = "build/"
